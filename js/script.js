8441ade0c415a7f689a8805de4aef5ba22510df8
sofiaroman1969 <maria.roman@tecdesoftware.edu.mx>
2025-11-18 13:32:50 -0600

    Ejemplo código

---
+++ js/script.js
@@ -0,0 +1,8 @@
+console.log("Sitio cargado correctamente"); 
+document.addEventListener("DOMContentLoaded", function() { 
+const form = document.querySelector("form"); 
+form.addEventListener("submit", function(event) { 
+event.preventDefault(); 
+alert("¡Gracias por tu mensaje!"); 
+}); 
+});
